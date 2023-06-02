cc89b7027c0caa2f30c74e3e52722797680f3af4
fgdamasceno <fgdamasceno@gmail.com>
2023-06-02 11:04:46 -0300

    dia 02/26

---
+++ projects/01-Fundamentals-Part-1/starter/codingChallenge#1.js
@@ -0,0 +1,17 @@
+// BMI Calculator
+
+const markMass = 78;
+const markHeight = 1.69;
+
+const johnMass = 95;
+const johnHeight = 1.88;
+
+const markBMI = markMass / markHeight ** 2;
+const johnBMI = johnMass / johnHeight ** 2;
+
+const markHigherBMI = markBMI > johnBMI;
+const johnHigherBMI = johnBMI > markBMI;
+
+console.log(markBMI, johnBMI);
+console.log(markHigherBMI);
+console.log(johnHigherBMI);
--- projects/01-Fundamentals-Part-1/starter/script.js
+++ projects/01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,64 @@
-let js = "amazing";
-// if (js === "amazing") alert("JavaScript is FUN!");
-console.log(40 + 8 + 23 - 10);
+// let js = "amazing";
+// // if (js === "amazing") alert("JavaScript is FUN!");
+// console.log(40 + 8 + 23 - 10);
 
-let firstName = "Jonas";
-console.log(firstName);
+// let firstName = "Jonas";
+// console.log(firstName);
+
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1991;
+// birthYear = 1990; // returns an error. const is not supposed to change
+
+// const job; // not legal in js. const can not be declared without a defined value!
+
+// var job = 'programmer';
+// job = 'teacher';
+// // avoid using
+
+// // OPERATORS
+// // Mathematical
+// const now = 2037
+// const ageJonas = now - 1991; // +, -, *, /, **, ++, --, % (mathematical operator)
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+// const firstName = 'Jonas';
+// const lastName = 'Schmedtmann';
+// console.log(firstName + ' ' + lastName);
+
+// let x = 10 + 5; // =, +=, -=, *=, /=n, **=, %= (assignment operator)
+// x += 10;
+// console.log(x);
+
+// // Comparison
+// console.log(ageJonas > ageSarah);
+// // >, <, >=, <=, !=, ==, ===,  
+
+// OPERATOR PRECEDENCE
+// ver mdn
+
+// // STRINGS AND TEMPLATE LITERALS
+// const firstName = 'Jonas';
+// const jog = 'teacher';
+// const birthYear = 1991;
+// const year = 2037;
+
+// const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!');
+// console.log(jonas);
+
+// // template literals - much better way of concatenating strings. introduced in ES6 (`${}`)
+// const jonasNew = `I'm ${firstName} a ${year - birthYear} year old ${job}!`;
+
+// // multiple lines before:
+// console.log('String with \n\
+// multiple \n\
+// lines');
+
+// // multiple lines after ES6: just hitting ENTER key and using backticks
+// console.log(`String
+// multiple
+// line`);
