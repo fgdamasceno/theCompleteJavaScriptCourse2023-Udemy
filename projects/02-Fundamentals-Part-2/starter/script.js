9ef7807ef4d6a403fb505ab4d39da57886b0c8aa
Fernando G. Damasceno <fgdamasceno@gmail.com>
2023-06-07 21:57:03 -0300

    dia 07

--- projects/02-Fundamentals-Part-2/starter/script.js
+++ projects/02-Fundamentals-Part-2/starter/script.js
@@ -731,6 +731,27 @@ if (mark.bmi > john.bmi) {
 
 // Use the calcTip function we wrote before (included in the starter code) to calculate tips and total values (bill + tip) for every bill value in the bills array. Use a for loop to perform the 10 calculations!
 
-
-
 // TEST DATA: 22, 295, 176, 440, 37, 105, 10, 1100, 86, and 52.
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+for (let i = 0; i < bills.length; i++) {
+  tips.push(calcTip(bills[i]));
+  totals.push(tips[i] + bills[i]);
+}
+
+function calcAverage(arr) {
+  let sum = 0; // Tem que ser criada fora do loop
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i]
+  }
+  return sum / arr.length;
+}
+
+console.log(calcAverage(totals));
