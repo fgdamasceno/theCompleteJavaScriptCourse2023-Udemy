592d417a35570ff660d2b23fad0cf8fc3f392250
Fernando G. Damasceno <fgdamasceno@gmail.com>
2023-06-08 10:09:02 -0300

    dia 8

--- projects/03-Developer-Skills/starter/script.js
+++ projects/03-Developer-Skills/starter/script.js
@@ -1,3 +1,35 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+// 55. SETTING UP PRETTIER AND VS CODE
+// Editing Prettier;
+// Configuring VSCode to print 'console.log();' with a snippet;
+//      type: log
+
+//////////////////////
+
+// 56. INSTALLING NODE.JS AND SETTING UP A DEV ENVIRONMENT
+// Installing NODE.JS to professionally view the page we are working on. A professional alternative to 'Live Server';
+// https://nodejs.org/en/download > windows installer > install > restart vscode > npm install live-server -g > after installation type live-server. Make sure you are in the right folder where your .html file is.
+// npm = node package manager
+
+//////////////////////
+
+// 57. LEARNING HOW TO CODE
+// A good lecture that could easily be watched again. About how to become a real developer, the true difficult way and how to survive it and grow to be a real professional in the field.
+
+//////////////////////
+
+// 58. HOW TO THINK LIKE A DEVELOPER: BECOME A PROBLEM SOLVER!
+// How to think like a developer (solving problem [not the bugs]). 4 STEPS FRAMEWORK:
+// - Make sure you 100% understand the problem. Ask the right questions to get a clear picture of the problem
+// - Divide and conquer: Break a big problem into smaller sub-problems
+// - Don't be afraid to do as much research as you have to (google, stackOverflow, MDN)
+// - For bigger problems, write pseudo-code before writing the actual code
+
+//////////////////////
+
+// 59. USING GOGGLE, STACKOVERFLOW AND MDN
+
+
+//////////////////////
